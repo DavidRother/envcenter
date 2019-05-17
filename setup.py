f775ac82a3f540c896e75ee62dff3194cc98ff0b
David Rother <david@edv-drucksysteme.de>
2019-05-17 09:39:34 +0200

    initial python packaging repo push

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='envcenter',
+      version='0.1',
+      description='System Management to streamline access to data sources and piping data to models',
+      url='https://github.com/DavidRother/envcenter',
+      author='David Rother',
+      author_email='david@edv-drucksysteme.de',
+      license='MIT',
+      packages=['envcenter'],
+      zip_safe=False)
